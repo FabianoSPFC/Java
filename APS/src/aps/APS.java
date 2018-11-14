59938e267b97742db644aebe02e370cbe1ee516a
FabianoSPFC <programador.workspace@gmail.com>
2018-11-14 13:41:57 -0300

    Conversão de Temperatura com swing

---
+++ APS/src/aps/APS.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aps;
+
+/**
+ *
+ * @author Fabiano Filho
+ */
+public class APS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Interface1 frame = new Interface1();
+        frame.setVisible(true);
+    }
+    
+}
